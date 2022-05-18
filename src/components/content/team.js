8e8b9d9c77c22182b74288b3f9dbcf9ed2cb463c
Benjamin Kilimnik <benjaminkilimnik@gmail.com>
2022-05-18 00:30:31 -0400

    add contact

--- src/components/content/team.js
+++ src/components/content/team.js
@@ -1,21 +1,21 @@
 import {
   bears2,
-//   // sbudnicN,
-//   sbudnicNW,
-//   theo,
-//   rick,
-//   dheraj,
-//   ashton,
-//   daniel,
-//   haley,
-//   marco,
-//   filip,
-//   maddie,
-//   benji,
-//   selia,
-//   lia,
-//   jenn,
-//   gabby
+  //   // sbudnicN,
+  //   sbudnicNW,
+  //   theo,
+  //   rick,
+  //   dheraj,
+  //   ashton,
+  //   daniel,
+  //   haley,
+  //   marco,
+  //   filip,
+  //   maddie,
+  //   benji,
+  //   selia,
+  //   lia,
+  //   jenn,
+  //   gabby
 } from "../content/images"
 
 const team = [
@@ -26,24 +26,22 @@ const team = [
       "A junior at Brown University studying computer science with a focus on systems research, data science, and web design. When I'm not coding up a hot mess in the Computer Science department, you can find me working at the Center for Computation and Visualization, researching for Systems@Brown or reading historical fiction next to a radiator.",
     team: "leadership",
     category: "",
-    position: "Co-Captain",
+    position: "Co-Captain, benjamin_kilimnik@brown.edu",
     image: bears2,
   },
   {
     id: 2,
-    title: "Lachlan MacKenzie",
-    subtitle:
-      "",
+    title: "Harry Wang",
+    subtitle: "",
     team: "leadership",
     category: "",
-    position: "Co-Captain",
+    position: "Co-Captain, yuanhao_wang@brown.edu",
     image: bears2,
   },
   {
     id: 3,
-    title: "Kush Patel",
-    subtitle:
-      "",
+    title: "Lachlan MacKenzie",
+    subtitle: "",
     team: "leadership",
     category: "",
     position: "Co-Captain",
@@ -52,8 +50,7 @@ const team = [
   {
     id: 4,
     title: "Wayne Luan",
-    subtitle:
-      "",
+    subtitle: "",
     team: "leadership",
     category: "",
     position: "Chief Designer",
